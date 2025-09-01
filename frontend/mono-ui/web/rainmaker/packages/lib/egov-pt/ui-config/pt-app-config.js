ba73f21514b5963f0f7630e22357062a54b34ddc
umesh-sspl <umesh@sparrowsoftech.net>
2025-09-01 11:22:14 +0530

    removed mono-ui

--- frontend/mono-ui/web/rainmaker/packages/lib/egov-pt/ui-config/pt-app-config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var defaultLocation = exports.defaultLocation = { lat: 30.7333, lng: 76.7794 };
--- frontend/mono-ui/web/rainmaker/packages/lib/egov-ui-kit/redux/workFlow/actionTypes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var FETCH_BUISNESS_SERVICE = exports.FETCH_BUISNESS_SERVICE = "FETCH_BUISNESS_SERVICE";
